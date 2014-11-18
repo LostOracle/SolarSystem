b76a4052dbcfa714df030d4b03fce8e72f679bfc
CsmithSD <Chris.SDSMT@gmail.com>
2014-11-18 16:38:07 -0700

    fixed error

--- include/Planet.h
+++ include/Planet.h
@@ -8,7 +8,7 @@ class Planet
            // Planet(const char* planet_name, double planet_radius, double orbital_radius, 
            //        double theta, double orbital_velocity, double rotation_speed, double tilt, 
            //       const double* color, const char* file_name );
-            Planet(planet_info info);
+            Planet(Planet_Info info);
             ~Planet();
 
             double r;
